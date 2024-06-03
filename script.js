117c80a3ca5f161b329f0cd654fdc732aa4a6c68
Mitri <hmirseid54@gmail.com>
2024-06-03 12:31:03 +0400

    project yarandi

---
+++ script.js
@@ -0,0 +1,137 @@
+const bale = document.getElementById('bale')
+const bale1 = document.getElementById('bale1')
+
+const benzema = document.getElementById('benzema')
+const benzema1 = document.getElementById('benzema1')
+
+const haland = document.getElementById('haland')
+const haland1 = document.getElementById('haland1')
+
+const kane = document.getElementById('kane')
+const kane1 = document.getElementById('kane1')
+
+const mbappe = document.getElementById('mbappe')
+const mbappe1 = document.getElementById('mbappe1')
+
+const messi = document.getElementById('messi')
+const messi1 = document.getElementById('messi1')
+
+const muller = document.getElementById('muller')
+const muller1 = document.getElementById('muller1')
+
+const neymar = document.getElementById('neymar')
+const neymar1 = document.getElementById('neymar1')
+
+const rodrygo = document.getElementById('rodrygo')
+const rodrygo1 = document.getElementById('rodrygo1')
+
+const ronaldo = document.getElementById('ronaldo')
+const ronaldo1 = document.getElementById('ronaldo1')
+
+const valverde = document.getElementById('valverde')
+const valverde1 = document.getElementById('valverde1')
+
+const vinisius = document.getElementById('vinisius')
+const vinisius1 = document.getElementById('vinisius1')
+const div = document.querySelectorAll('div')
+
+if (true) {
+    bale.addEventListener('click' , () => {
+        bale.style.backgroundImage = 'url(bale.jpeg)'
+        bale.style.backgroundSize = 'cover'
+    })
+    bale1.addEventListener('click' , () => {
+        bale1.style.backgroundImage = 'url(bale.jpeg)'
+        bale1.style.backgroundSize = 'cover'
+    })
+    benzema.addEventListener('click' , () => {
+        benzema.style.backgroundImage = 'url(benzema.jpeg)'
+        benzema.style.backgroundSize = 'cover'
+    })
+    benzema1.addEventListener('click' , () => {
+        benzema1.style.backgroundImage = 'url(benzema.jpeg)'
+        benzema1.style.backgroundSize = 'cover'
+    })
+    haland.addEventListener('click' , () => {
+        haland.style.backgroundImage = 'url(haland.jpeg)'
+        haland.style.backgroundSize = 'cover'
+    })
+    haland1.addEventListener('click' , () => {
+        haland1.style.backgroundImage = 'url(haland.jpeg)'
+        haland1.style.backgroundSize = 'cover'
+    })
+    kane.addEventListener('click' , () => {
+        kane.style.backgroundImage = 'url(kane.jpeg)'
+        kane.style.backgroundSize = 'cover'
+    })
+    kane1.addEventListener('click' , () => {
+        kane1.style.backgroundImage = 'url(kane.jpeg)'
+        kane1.style.backgroundSize = 'cover'
+    })
+    mbappe.addEventListener('click' , () => {
+        mbappe.style.backgroundImage = 'url(mbappe.jpeg)'
+        mbappe.style.backgroundSize = 'cover'
+    })
+    mbappe1.addEventListener('click' , () => {
+        mbappe1.style.backgroundImage = 'url(mbappe.jpeg)'
+        mbappe1.style.backgroundSize = 'cover'
+    })
+    messi.addEventListener('click' , () => {
+        messi.style.backgroundImage = 'url(messi.jpeg)'
+        messi.style.backgroundSize = 'cover'
+    })
+    messi1.addEventListener('click' , () => {
+        messi1.style.backgroundImage = 'url(messi.jpeg)'
+        messi1.style.backgroundSize = 'cover'
+    })
+    muller.addEventListener('click' , () => {
+        muller.style.backgroundImage = 'url(muller.jpeg)'
+        muller.style.backgroundSize = 'cover'
+    })
+    muller1.addEventListener('click' , () => {
+        muller1.style.backgroundImage = 'url(muller.jpeg)'
+        muller1.style.backgroundSize = 'cover'
+    })
+    neymar.addEventListener('click' , () => {
+        neymar.style.backgroundImage = 'url(neymar.jpeg)'
+        neymar.style.backgroundSize = 'cover'
+    })
+    neymar1.addEventListener('click' , () => {
+        neymar1.style.backgroundImage = 'url(neymar.jpeg)'
+        neymar1.style.backgroundSize = 'cover'
+    })
+    rodrygo.addEventListener('click' , () => {
+        rodrygo.style.backgroundImage = 'url(rodrygo.jpeg)'
+        rodrygo.style.backgroundSize = 'cover'
+    })
+    rodrygo1.addEventListener('click' , () => {
+        rodrygo1.style.backgroundImage = 'url(rodrygo.jpeg)'
+        rodrygo1.style.backgroundSize = 'cover'
+    })
+    ronaldo.addEventListener('click' , () => {
+        ronaldo.style.backgroundImage = 'url(ronaldo.jpeg)'
+        ronaldo.style.backgroundSize = 'cover'
+    })
+    ronaldo1.addEventListener('click' , () => {
+        ronaldo1.style.backgroundImage = 'url(ronaldo.jpeg)'
+        ronaldo1.style.backgroundSize = 'cover'
+    })
+    valverde.addEventListener('click' , () => {
+        valverde.style.backgroundImage = 'url(valverde.jpeg)'
+        valverde.style.backgroundSize = 'cover'
+    })
+    valverde1.addEventListener('click' , () => {
+        valverde1.style.backgroundImage = 'url(valverde.jpeg)'
+        valverde1.style.backgroundSize = 'cover'
+    })
+    vinisius.addEventListener('click' , () => {
+        vinisius.style.backgroundImage = 'url(vinisius.jpeg)'
+        vinisius.style.backgroundSize = 'cover'
+    })
+    vinisius1.addEventListener('click' , () => {
+        vinisius1.style.backgroundImage = 'url(vinisius.jpeg)'
+        vinisius1.style.backgroundSize = 'cover'
+    })
+}else{
+    div.style.backgroundColor ='rgba(0, 0, 0, 0.84)'
+}
